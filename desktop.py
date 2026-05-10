6b541dbad29db1ee7e384cfeadbaa4778c895980
Nikolas-Herba <nikolasherba2005@gmail.com>
2026-05-10 17:42:48 -0300

    feat: tirendo os emojis

--- desktop.py
+++ desktop.py
@@ -3,18 +3,36 @@ import subprocess
 import time
 import requests
 import webview
+import os
+import sys
 
 
 DJANGO_URL = "http://127.0.0.1:8000"
+
+
+def resource_path(relative_path):
+    """
+    Resolve caminhos no PyInstaller
+    """
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 
 def start_django():
     """
     Inicia o servidor Django
     """
-    subprocess.run(
-        ["python", "manage.py", "runserver"],
-        shell=True
+    manage_path = resource_path("manage.py")
+
+    subprocess.Popen(
+        [sys.executable, manage_path, "runserver"],
+        cwd=resource_path("."),
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
     )
 
 
@@ -30,7 +48,7 @@ django_thread.start()
 # espera servidor iniciar
 server_started = False
 
-for _ in range(15):
+for _ in range(20):
     try:
         requests.get(DJANGO_URL)
         server_started = True
